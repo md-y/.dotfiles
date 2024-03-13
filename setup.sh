c386d64c11b93162d7d94f4dc75e008d595db6f8
md-y <15069105+md-y@users.noreply.github.com>
2024-03-12 20:32:59 -0600

    Simplified setup.sh

--- setup.sh
+++ setup.sh
@@ -2,11 +2,10 @@
 
 pushd home
 
-# Find and remove all potential conflicting files within $HOME
-# This is needed especially for MSYS2 since stow can't identify symlinks
-find . \( -type f -o -type l \) | cut -d '/' -f3- | sed "s|^|$HOME/|" | xargs -I{} rm -vf {}
+echo "Resetting current links:"
+stow -D */ -vt "$HOME"
 
-echo "Stowing home"
-stow */ -t "$HOME" 
+echo "Stowing home:"
+stow */ -vt "$HOME" 
 
 popd
